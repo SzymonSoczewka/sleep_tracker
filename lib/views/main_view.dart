390009d7094efd1b7c93b09a8cc59870916a9588
Szymon Soczewka <42994488+Sajmonoo1998@users.noreply.github.com>
2020-04-28 00:58:02 +0200

    debug mode banner removed

--- lib/views/main_view.dart
+++ lib/views/main_view.dart
@@ -11,6 +11,7 @@ class MainView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           appBar: AppBar(
             title: Text('Sleep Tracker'),
